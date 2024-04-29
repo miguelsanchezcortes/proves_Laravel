c8ec62bf37eff7ca6601f4a7f267652e6a10e9b1
Miguel Sánchez <miguel.sanchez@cirvianum.cat>
2024-04-29 16:30:26 +0200

    instal·lat breeze

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,20 @@
 <?php
 
-use App\Http\Controllers\MpController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', [MpController::class, 'hello']);
-Route::get('/bye', [MpController::class, 'bye']);
-Route::get('/user/create', [UserController::class, 'create']);
-Route::post('user/store', [UserController::class, 'store']);
-Route::get('/user/login', [UserController::class, 'login']);
-Route::post('/user/login',[UserController::class, 'authenticate']);
-Route::get('/user/list', [UserController::class, 'list']);
-Route::get('/user/edit/{id}', [UserController::class, 'edit']);
-Route::get('/user/delete/{id}', [UserController::class, 'delete']);
-Route::post('/user/update/{id}',[UserController::class, 'update']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
